5dc43a2b04764af4334abb5f1e334b26e2e5569a
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-03-14 19:45:12 +0100

    Initial commit

---
+++ addHoroscope.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+
+<!--
+sidan ska bara gå att begära via POST, den ska kolla efter ett födelsedatum i $_POST, räkna ut 
+vilket horoskop födelsedatumet tillhör och spara det i $_SESSION.
+Om ett horoskop redan finns sparat ska det inte skrivas över. Om det inte gick att räkna ut 
+horoskopet ska ingenting sparas.
+   
+Sidan ska inte använda echo eller skriva någon output förutom true eller false, beroende 
+på om horoskopet sparades.
+-->
+<?php
+
+
+
+
+
+?>
+    
+</body>
+</html>
---
+++ updateHoroscope.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+
+<!--
+sidan ska bara gå att begära via PUT, den ska kolla efter ett födelsedatum i PUT datan, det finns 
+ingen inbyggd $_PUT men ni kan skapa den själva genom att köra denna raden längst upp i filen:
+parse_str(file_get_contents("php://input"), $_PUT);
+räkna ut vilket horoskop födelsedatumet tillhör och uppdaterade det sparade horoskopet som 
+finns i $_SESSION och skriva ut true.
+Om inget horoskop finns i $_SESSION ska sidan inte uppdatera något och skriva ut false
+
+-->
+<?php
+
+
+
+
+
+?>
+    
+</body>
+</html>
