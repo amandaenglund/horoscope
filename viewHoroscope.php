57292de5495557075be8863a8de589b2da5e66d7
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-03-14 20:00:24 +0100

    Fått upp lite basic på index

---
+++ viewHoroscope.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+
+<!--
+sidan ska bara gå att begära via GET, den ska kolla om ett horoskop finns sparat i $_SESSION och i 
+så fall skriva ut det i output. Annars ska sidan inte skriva ut någonting.
+-->
+<?php
+
+
+
+
+
+?>
+    
+</body>
+</html>
